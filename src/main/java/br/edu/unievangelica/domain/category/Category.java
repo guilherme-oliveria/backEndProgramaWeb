ea542daadbc2dd9da04378b74c84cc6f39fd48e9
Aluno <https://guilherme-oliveria@github.com>
2018-04-26 22:04:01 -0300

    att

--- src/main/java/br/edu/unievangelica/domain/category/Category.java
+++ src/main/java/br/edu/unievangelica/domain/category/Category.java
@@ -1,20 +1,31 @@
 package br.edu.unievangelica.domain.category;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
+@Getter
+@Setter
 @Entity
-@Table(name = "Categoria")
-@JsonIgnoreProperties
+@Table(name = "category")
 public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    private static final long serialversionUID = 1L;
-
-    @Id@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_seq")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_seq")
+    @SequenceGenerator(name = "category_id_seq", sequenceName = "category_id_seq", allocationSize = 1)
+    @Column(name = "id")
+    @Getter
     private long id;
 
+    @NotEmpty
+    @Size(max = 50)
+    @Column(name = "nome")
     private String nome;
+
 
 }
--- src/main/java/br/edu/unievangelica/domain/category/CategoryController.java
+++ src/main/java/br/edu/unievangelica/domain/category/CategoryController.java
@@ -1,36 +1,42 @@
 package br.edu.unievangelica.domain.category;
 
+import br.edu.unievangelica.core.controller.ResponseAbstractController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-public class CategoryController  {
-
-    @Autowired
-    CategoryRepository categoryRepository;
+@RestController
+@RequestMapping(value = "/category")
+public class CategoryController extends ResponseAbstractController {
 
     @Autowired
     CategoryService categoryService;
 
-    public List<Category> findAll(){
-        return  categoryRepository.findAll();
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return jsonResponse(categoryService.findAll());
     }
 
-    public Category findById(long id){
-        return categoryRepository.findOne(id);
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> findOne(@PathVariable long id) {
+        return jsonResponse(categoryService.findOne(id));
     }
 
-    public Boolean delete(long id){
-        categoryRepository.delete(id);
-        return true;
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.OK);
     }
 
-    public ResponseEntity<?> save(@Validated @RequestBody Category category){
-        return jsonResponse(categoryService.save(category));
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable long id, @RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.update(id, category), HttpStatus.OK);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return jsonResponse(categoryService.delete(id));
+    }
 
 }
